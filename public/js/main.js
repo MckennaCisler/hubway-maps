001c0a99a8544e73d24ce2ac756520440f8ff3a6
MckennaCisler <mckennacisler@gmail.com>
2017-03-05 02:00:50 -0500

    Add table back

--- public/js/main.js
+++ public/js/main.js
@@ -7,6 +7,20 @@ $(document).ready(() => {
     $.post("/stations",  responseJSON => {
         stations = responseJSON;
         console.log(responseJSON);
+        
+        d3.select("#station-table > tbody")
+            .selectAll("tr")
+            .data(responseJSON)
+            .enter().append("tr")
+              .html(function(data) {
+                var innerHTML = "";
+                    for (let stationProp in data) {
+                        if (data.hasOwnProperty(stationProp)) {
+                            innerHTML += "<td>" + data[stationProp] +"</td>";
+                        }
+                    }
+                    return innerHTML;
+                });
     });
 
 
