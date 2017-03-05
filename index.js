33553c55e11d6890c4fbeba9601e58a4c9df2f54
MckennaCisler <mckennacisler@gmail.com>
2017-03-05 01:55:52 -0500

    Add index changes to master branch

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var app = express();
 app.use(express.static('public'));
 
 const STATIONS_FILE = 'public/data/hubway_stations.csv';
-const STATIONS_HEADERS = ["id", "terminal", "station", "municipal", "lat", "lng", "status"];
 
 // data
 var stations = [];
@@ -17,22 +16,34 @@ app.post("/stations", function(req, res) {
 
 app.listen(3000, function () {
 	console.log("Loading stations csv...");
-	
-	// Create the parser
-	var parser = parse({delimiter: ','});
-	
-	var input = fs.createReadStream(STATIONS_FILE);
-	input.pipe(parser)
-	.on('data', function(csvrow){
-		station = {};
-		for (var i = 0; i < STATIONS_HEADERS.length; i++) {
-			station[STATIONS_HEADERS[i]] = csvrow[i];
-		}
-		stations.push(station);
-	})
-    .on('end',function() {
+	loadCSV(STATIONS_FILE, function(results) {
+		stations = results;
 		console.log("Finished loading stations!");
-    });
+	});
 	
 	console.log('Hubway listening on port 3000!');
 });
+
+function loadCSV(file, callback) {
+	var headers;
+	var results = [];
+	
+	var parser = parse({delimiter: ','});
+	
+	var input = fs.createReadStream(file);
+	input.pipe(parser)
+	.on('data', function(csvrow) {
+		if (headers === undefined) {
+			headers = csvrow;
+		} else {
+			row = {};
+			for (var i = 0; i < csvrow.length; i++) {
+				row[headers[i]] = csvrow[i];
+			}
+			results.push(row);
+		}
+	})
+    .on('end',function() {
+		callback(results);
+    });
+}
