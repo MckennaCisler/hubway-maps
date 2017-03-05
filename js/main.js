45883a375ee7efbe38e5758fa38a9301e5bfa16b
MckennaCisler <mckennacisler@gmail.com>
2017-03-05 05:51:03 -0500

    add graph

--- js/main.js
+++ js/main.js
@@ -25,7 +25,38 @@ $(document).ready(() => {
     document.onmousemove = function(e) {
       div.attr("style", "left:" + (e.pageX + 25) + "px;top:" + e.pageY + "px");
     };
-
+    
+    // graph
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+      
+    var max_poverty_rate = 26.5;
+    var max_stations_per = 4;
+    
+    // add the graph canvas to the body of the webpage
+    var svgGraph = d3.select("#bar-chart").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+          
+    // setup x 
+    var xValue = function(d) { return d.properties.stations * 100;}, // data -> value
+        xScale = d3.scaleLinear().range([0, width]), // value -> display
+        xMap = function(d) { return xScale(xValue(d));}, // data -> display
+        xAxis = d3.axisBottom().scale(xScale);
+    
+    // setup y
+    var yValue = function(d) { return d.properties.poverty_rate * 10000;}, // data -> value
+        yScale = d3.scaleLinear().range([height, 0]), // value -> display
+        yMap = function(d) { return yScale(yValue(d));}, // data -> display
+        yAxis = d3.axisLeft().scale(yScale);
+        
+    // don't want dots overlapping axis, so add in buffer to data domain
+    xScale.domain([0, 4]);
+    yScale.domain([0, max_poverty_rate]);
+        
     // Load map data
     d3.json('data/complete_data.json', function(error, mapData) {
       if (error) {
@@ -58,7 +89,64 @@ $(document).ready(() => {
               
               div.classed("hidden", true);
            });
-      }
+
+        // x-axis
+        svgGraph.append("g")
+           .attr("class", "x axis")
+           .attr("transform", "translate(0," + height + ")")
+           .call(xAxis)
+         .append("text")
+           .attr("class", "label")
+           .attr("x", width)
+           .attr("y", -6)
+           .style("text-anchor", "end")
+           .text("Calories");
+        
+        // y-axis
+        svgGraph.append("g")
+           .attr("class", "y axis")
+           .call(yAxis)
+         .append("text")
+           .attr("class", "label")
+           .attr("transform", "rotate(-90)")
+             .attr("y", 6)
+             .attr("dy", ".71em")
+             .style("text-anchor", "end")
+             .text("Protein (g)");
+        
+        // dots
+        svgGraph.selectAll(".dot")
+           .data(features)
+           .enter().append("circle")
+           .attr("class", "dot")
+           .attr("r", 5)
+           .attr("cx", function(d) {
+               return d.properties.stations * (width / max_stations_per);
+           })
+           .attr("cy", function(d) {
+               return (height / max_poverty_rate) * (max_poverty_rate - d.properties.poverty_rate * 100);
+           })
+           .style("fill", "#000")
+           .on("mouseover", function(d) {
+               d3.select(this)
+                   .transition()
+                     .duration(HOVER_TRANS_MS)
+                     .attr("r",10);
+                     
+               
+               div.classed("hidden", false);
+               div.style("opacity", 1);
+               div.html(getTractTooltip(d));
+             })
+           .on("mouseout", function(d) {
+               d3.select(this)
+                   .transition()
+                     .duration(HOVER_TRANS_MS)
+                     .attr("r", 5);
+               
+               div.classed("hidden", true);
+            });
+         }
 
       // nest because the first elements put onto an SVG show up on top
       d3.json("data/hubway_stations.geo.json", function(error, statData) {
